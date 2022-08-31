739957292054f2f6d44a74899b26620827144085
alazarLaz <alazar.yeh@gmail.com>
2022-08-31 18:39:14 +0300

    Complete

--- src/App.js
+++ src/App.js
@@ -1,60 +1,68 @@
 import Palette from "./Palette";
-import ColorSeed from './colorSeed'
-import generatePalette from './colorHelper'
-import { Route, Switch } from 'react-router-dom'
+import ColorSeed from "./colorSeed";
+import generatePalette from "./colorHelper";
+import { Route, Switch } from "react-router-dom";
 import React, { Component } from "react";
 import Palettelist from "./Palettelist";
 import SingleColorPalette from "./SingleColorPalette";
 import NewPaletteForm from "./NewPaletteForm";
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       
-    }
-    this.findPalette = this.findPalette.bind(this)
-  }
+	constructor(props) {
+		super(props);
 
-  findPalette(id)
-  {
-    return ColorSeed.find(color=>color.id === id)
-  }
-  
+		this.state = {};
+		this.findPalette = this.findPalette.bind(this);
+	}
 
-  render()
-  {
-    return(
-      <div>
-      <Switch>
-        <Route exact path="/" render={(routeProps)=>
-          <Palettelist palettes = {ColorSeed} {...routeProps}/>
-        }>
-        </Route>
-        <Route exact path="/palette/new" render={()=><NewPaletteForm />} >
-        </Route>
-        <Route 
-          exact 
-          path = "/palette/:id" 
-          render = {(routeProps) => 
-        <Palette palette = { generatePalette(this.findPalette(routeProps.match.params.id)) }/>
-        }>
-        </Route>
-        <Route
-          path="/palette/:paletteId/:colorId" 
-          render={(routeProps)=>
-            // <h1>SingleColor</h1>
-          <SingleColorPalette 
-            colorId = { routeProps.match.params.colorId } 
-            palette = { generatePalette(this.findPalette(routeProps.match.params.paletteId)) } />
-          }>
-        </Route>
-      </Switch>
-      {/* <Palette palette = {generatePalette(ColorSeed[3])}/> */}
-    </div>
-    )
-  }
+	findPalette(id) {
+		return ColorSeed.find((color) => color.id === id);
+	}
+
+	render() {
+		return (
+			<div>
+				<Switch basename="/colors-app">
+					<Route
+						exact
+						path="/"
+						render={(routeProps) => (
+							<Palettelist palettes={ColorSeed} {...routeProps} />
+						)}
+					></Route>
+					<Route
+						exact
+						path="/palette/new"
+						render={() => <NewPaletteForm />}
+					></Route>
+					<Route
+						exact
+						path="/palette/:id"
+						render={(routeProps) => (
+							<Palette
+								palette={generatePalette(
+									this.findPalette(routeProps.match.params.id)
+								)}
+							/>
+						)}
+					></Route>
+					<Route
+						path="/palette/:paletteId/:colorId"
+						render={(routeProps) => (
+							// <h1>SingleColor</h1>
+							<SingleColorPalette
+								colorId={routeProps.match.params.colorId}
+								palette={generatePalette(
+									this.findPalette(routeProps.match.params.paletteId)
+								)}
+							/>
+						)}
+					></Route>
+				</Switch>
+				{/* <Palette palette = {generatePalette(ColorSeed[3])}/> */}
+			</div>
+		);
+	}
 }
 
 export default App;
